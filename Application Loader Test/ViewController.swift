6b0cc058da24a0835e528731b6e24833961f23c1
Akos Birmacher <akos.birmacher@bitrise.io>
2018-10-01 16:05:32 +0200

    Initial Commit

---
+++ Application Loader Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Application Loader Test
+//
+//  Created by Akos Birmacher on 2018. 10. 01..
+//  Copyright © 2018. Bitrise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
